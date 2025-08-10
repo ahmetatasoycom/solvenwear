8527ac91eb1d26a168530b7d78736a6250fd7031
ahmetatasoycom <fortnitey914@gmail.com>
2025-08-10 21:09:03 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./src/**/*.{html,js,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
